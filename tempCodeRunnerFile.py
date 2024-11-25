e4349c85c03619ffd0221c3364e4867dfabca51e
unknown <msupran17@gmail.com>
2024-11-25 22:53:22 +0545

    Cafe Chatbot

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+from nltk.stem.porter import PorterStemmer
+stemmer=PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+a="How long does shipping take?"
+print(a)
+a=tokenize(a)
+print(a)
